69f4e408c21540759c2dd3789932548f595cad81
AdrianaVlad <100144132+AdrianaVlad@users.noreply.github.com>
2023-02-24 19:02:08 +0200

    Add files via upload

---
+++ Lab1/Homework/src/main/java/com/mycompany/homework/Homework.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.homework;
+
+/**
+ *
+ * @author Vlad Adriana
+ */
+public class Homework {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
