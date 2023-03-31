166d3533efcfe901d82d4599d8782217f820c101
AdrianaVlad <100144132+AdrianaVlad@users.noreply.github.com>
2023-03-31 13:50:28 +0300

    Add files via upload

---
+++ Lab 6/Compulsory/Compulsory.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.compulsory;
+
+
+/**
+ *
+ * @author Vlad Adriana
+ */
+public class Compulsory {
+
+    public static void main(String[] args){
+       new MainFrame().setVisible(true);
+    }
+}
